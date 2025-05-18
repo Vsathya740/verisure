adee9d8b0deaaa7b7899faa0a7b62e84308b5cb8
Vsathya740 <72496339+Vsathya740@users.noreply.github.com>
2025-05-18 05:45:17 -0700

    sql

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,15 +1,28 @@
 import { Sequelize } from 'sequelize-typescript';
-import { VerifierObservations } from './sequelize/models/VerifierObservations';
-import { ApplicationRegistration } from './sequelize/models/ApplicationRegistration';
-import { CustomerContactReport } from './sequelize/models/CustomerContactReport';
-import { EmploymentVerificationReport } from './sequelize/models/VerificationReport';
+import { OrganisationMaster } from './sequelize/models/OrganisationMaster';
+import { BankMaster } from './sequelize/models/BankMaster';
+import { BankBranchMaster } from './sequelize/models/BankBranchMaster';
+import { UserMaster } from './sequelize/models/UserMaster';
+import { Application } from './sequelize/models/Application';
+import { ResidenceVerification } from './sequelize/models/ResidenceVerification';
+import { EmployerVerification } from './sequelize/models/EmployerVerification';
+import { TelephoneVerification } from './sequelize/models/TelephoneVerification';
 
 export const sequelize = new Sequelize({
   dialect: 'mysql',
   host: process.env.DB_HOST || 'localhost',
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'IhmOhm@93',
+  password: process.env.DB_PASS || 'password',
   database: process.env.DB_NAME || 'verisure',
-  models: [VerifierObservations, ApplicationRegistration, CustomerContactReport, EmploymentVerificationReport],
+  models: [
+    OrganisationMaster,
+    BankMaster,
+    BankBranchMaster,
+    UserMaster,
+    Application,
+    ResidenceVerification,
+    EmployerVerification,
+    TelephoneVerification
+  ],
   logging: false
 });
