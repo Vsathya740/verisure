421fd30a24634422051193b23d9799c0d2b10ca8
Vsathya740 <72496339+Vsathya740@users.noreply.github.com>
2025-05-18 23:27:07 -0700

    roles

--- src/sequelize/migrations/20240321000004-add-role-to-user-master.ts
+++ src/sequelize/migrations/20240321000004-add-role-to-user-master.ts
@@ -2,11 +2,14 @@ import { QueryInterface, DataTypes } from 'sequelize';
 
 export = {
   up: async (queryInterface: QueryInterface) => {
-    await queryInterface.addColumn('user_master', 'role', {
-      type: DataTypes.ENUM('MANAGER', 'AGENT'),
-      defaultValue: 'AGENT',
-      allowNull: false
-    });
+    const tableInfo = await queryInterface.describeTable('user_master');
+    if (!tableInfo.role) {
+      await queryInterface.addColumn('user_master', 'role', {
+        type: DataTypes.ENUM('MANAGER', 'AGENT'),
+        defaultValue: 'AGENT',
+        allowNull: false
+      });
+    }
   },
 
   down: async (queryInterface: QueryInterface) => {
